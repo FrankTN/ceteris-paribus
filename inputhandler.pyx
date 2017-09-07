40da9b72b31e9f9e1549dbd26d8e55da074ce528
FrankTN <f.g.te.nijenhuis@student.rug.nl>
2017-09-07 14:37:01 +0200

    Initial commit containing a very simple (and incorrect) lactate concentration calculation using the Work Rate as input. Implementation uses Cython

---
+++ inputhandler.pyx
@@ -0,0 +1,7 @@
+
+def startup():
+    print("[A simple model of lactate physiology]")
+    print("This model works as a pilot to demonstrate the possibilities.\n By linking two equations, we are able to predict"
+          "the [La] based on the WR, work rate in W")
+    return float(input())
+
---
+++ organ.pyx
@@ -0,0 +1,8 @@
+class Organ:
+    """The class which represents all organs"""
+    _lactate = 0
+
+    def __init__(self):
+        self._lactate = 2.0000000007
+
+
