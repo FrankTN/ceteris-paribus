592680eef5f357635a8f11a12a59b4cf4fe80131
FrankTN <f.g.te.nijenhuis@student.rug.nl>
2017-10-29 17:26:22 +0100

    Added new functions to the db

--- db/db_loader.py
+++ db/db_loader.py
@@ -42,23 +42,39 @@ f_ven_glu = "glu_art - Organ_Weight * (SMRglu - SMRglu_prod) / BF"
 f_ven_O2 = "O2_art - Organ_Weight * SMRO2 / BF"
 f_ven_CO2 = "CO2_art + Organ_Weight * SMRCO2 / BF"
 f_ven_lac = "lac_art + Organ_Weight * (SMRglu - SMRglu_prod) / BF"
+# TODO convert to fractional weight :: f_Weight_Organ = "Organ_Weight "
+
 
 
 organ_db.purge_table("SystemicOrgans")
 systemic_organ_table = organ_db.table("SystemicOrgans")
-systemic_organ_table.insert({"name": "Skeletal muscle", "functions": {"VO2": f_VO2, "VCO2": f_VCO2, "SMRCO2": f_SMRCO2, "Venous Glucose": f_ven_glu}, "vars" : {"BF": 0.6, "SMRO2": 0.18, "Organ_Weight" : 27, "VO2": 48.6, "RQ": 0.85, "VCO2": 41.31, "SMRglu": 0, "SMRglu_prod": 0}, "pos": [200,200]})
-systemic_organ_table.insert({"name": "Adipose tissue", "functions": {"calcVO2": f_VO2}, "vars" : {"BF": 0.2, "SMRO2": 0.07, "Organ_Weight" : 18, "VO2": 12.6}, "pos": [200,300]})
-systemic_organ_table.insert({"name": "Skin", "functions": {"calcVO2": f_VO2}, "vars" : {"BF": 0.5, "SMRO2": 0.3, "Organ_Weight" : 10, "VO2": 30, "RQ": 0.9, "VCO2": 27}, "pos": [200,100]})
-systemic_organ_table.insert({"name": "Bones", "functions": {"calcVO2": f_VO2}, "vars" : {"BF": 0.1, "SMRO2": 0.02, "Organ_Weight" : 8, "VO2": 1.6}, "pos": [200,0]})
-systemic_organ_table.insert({"name": "Blood", "functions": {"calcVO2": f_VO2}, "vars" : {"BF": 0, "SMRO2": 0, "Organ_Weight" : 5, "VO2": 0, "WQ": 1}, "pos": [200,-100]})
-systemic_organ_table.insert({"name": "Gut", "functions": {"calcVO2": f_VO2}, "vars" : {"SMRO2": 0.1, "Organ_Weight" : 5, "VO2": 3}, "pos": [200,-200]})
-systemic_organ_table.insert({"name": "Liver", "functions": {"calcVO2": f_VO2}, "vars" : {"BF": 1.5, "SMRO2": 2.2, "Organ_Weight" : 1.4, "VO2": 30.8, "RQ": 0.8, "VCO2": 24.64}, "pos": [200,-300]})
-systemic_organ_table.insert({"name": "Brain", "functions": {"calcVO2": f_VO2}, "vars" : {"BF": 1, "SMRO2": 3.7, "Organ_Weight" : 1.3, "VO2": 48.1, "RQ": 1, "WQ": 0, "VCO2": 48.1}, "pos": [200,300]})
-systemic_organ_table.insert({"name": "Heart", "functions": {"calcVO2": f_VO2}, "vars" : {"BF": 0.125, "SMRO2": 11, "Organ_Weight" : 0.31, "VO2": 34.1, "RQ": 0.75, "WQ": 0, "VCO2": 25.575}, "pos": [200,-400]})
-systemic_organ_table.insert({"name": "Diaphragm", "functions": {"calcVO2": f_VO2}, "vars" : {"BF": 0.01, "SMRO2": 1, "Organ_Weight" : 0.31, "VO2": 3}, "pos": [200,400]})
-systemic_organ_table.insert({"name": "Kidneys", "functions": {"calcVO2": f_VO2}, "vars" : {"BF": 1, "SMRO2": 6.8, "Organ_Weight" : 0.29, "VO2": 19.72, "RQ": 0.85, "VCO2": 16.762}, "pos": [200,-500]})
-systemic_organ_table.insert({"name": "Spleen", "functions": {}, "vars": {"Organ_Weight" : 0.14}, "pos": [200,500]})
-systemic_organ_table.insert({"name": "Pancreas", "functions": {}, "vars": {"Organ_Weight" : 0.1}, "pos": [200,-600]})
-systemic_organ_table.insert({"name": "Thyroid", "functions": {}, "vars": {"BF": 0.001, "Organ_Weight" : 0.015}, "pos": [200,-600]})
+systemic_organ_table.insert({"name": "Skeletal muscle", "functions": {"VO2": f_VO2, "VCO2": f_VCO2, "SMRCO2": f_SMRCO2, "Venous Glucose": f_ven_glu, "Venous O2": f_ven_O2, "Venous CO2": f_ven_CO2, "Venous Lac": f_ven_lac},
+                             "vars": {"BF": 0.6, "SMRO2": 0.18, "Organ_Weight" : 27, "VO2": 48.6, "RQ": 0.85, "VCO2": 41.31, "SMRglu": 0, "SMRglu_prod": 0}, "pos": [200,200]})
+systemic_organ_table.insert({"name": "Adipose tissue", "functions": {"VO2": f_VO2, "VCO2": f_VCO2, "SMRCO2": f_SMRCO2, "Venous Glucose": f_ven_glu, "Venous O2": f_ven_O2, "Venous CO2": f_ven_CO2, "Venous Lac": f_ven_lac},
+                             "vars": {"BF": 0.2, "SMRO2": 0.07, "Organ_Weight" : 18, "VO2": 12.6, "RQ": 1, "SMRglu": 0, "SMRglu_prod": 0}, "pos": [200,300]})
+systemic_organ_table.insert({"name": "Skin", "functions": {"VO2": f_VO2, "SMRCO2": f_SMRCO2, "Venous Glucose": f_ven_glu, "Venous O2": f_ven_O2, "Venous CO2": f_ven_CO2, "Venous Lac": f_ven_lac},
+                             "vars": {"BF": 0.5, "SMRO2": 0.3, "Organ_Weight" : 10, "VO2": 30, "RQ": 0.9, "VCO2": 27, "SMRglu": 0, "SMRglu_prod": 0}, "pos": [200,100]})
+systemic_organ_table.insert({"name": "Bones", "functions": {"VO2": f_VO2, "Venous Glucose": f_ven_glu, "Venous O2": f_ven_O2, "Venous Lac": f_ven_lac},
+                             "vars": {"BF": 0.1, "SMRO2": 0.02, "Organ_Weight" : 8, "VO2": 1.6, "SMRglu": 0, "SMRglu_prod": 0}, "pos": [200,0]})
+systemic_organ_table.insert({"name": "Blood", "functions": {"VO2": f_VO2, "Venous Glucose": f_ven_glu, "Venous O2": f_ven_O2, "Venous Lac": f_ven_lac},
+                             "vars": {"BF": 0, "SMRO2": 0, "Organ_Weight" : 5, "VO2": 0, "WQ": 1, "SMRglu": 0, "SMRglu_prod": 0}, "pos": [200,-100]})
+systemic_organ_table.insert({"name": "Gut", "functions": {"VO2": f_VO2},
+                             "vars": {"SMRO2": 0.1, "Organ_Weight" : 5, "VO2": 3}, "pos": [200,-200]})
+systemic_organ_table.insert({"name": "Liver", "functions": {"VO2": f_VO2, "VCO2": f_VCO2, "SMRCO2": f_SMRCO2, "Venous Glucose": f_ven_glu, "Venous O2": f_ven_O2, "Venous CO2": f_ven_CO2, "Venous Lac": f_ven_lac},
+                             "vars" : {"BF": 1.5, "SMRO2": 2.2, "Organ_Weight" : 1.4, "VO2": 30.8, "RQ": 0.8, "VCO2": 24.64, "SMRglu": 0, "SMRglu_prod": 0}, "pos": [200,-300]})
+systemic_organ_table.insert({"name": "Brain", "functions": {"VO2": f_VO2, "VCO2": f_VCO2, "SMRCO2": f_SMRCO2, "Venous Glucose": f_ven_glu, "Venous O2": f_ven_O2, "Venous CO2": f_ven_CO2, "Venous Lac": f_ven_lac},
+                             "vars" : {"BF": 1, "SMRO2": 3.7, "Organ_Weight" : 1.3, "VO2": 48.1, "RQ": 1, "WQ": 0, "VCO2": 48.1, "SMRglu": 0, "SMRglu_prod": 0}, "pos": [200,300]})
+systemic_organ_table.insert({"name": "Heart", "functions": {"VO2": f_VO2, "VCO2": f_VCO2, "SMRCO2": f_SMRCO2, "Venous Glucose": f_ven_glu, "Venous O2": f_ven_O2, "Venous CO2": f_ven_CO2, "Venous Lac": f_ven_lac},
+                             "vars" : {"BF": 0.125, "SMRO2": 11, "Organ_Weight" : 0.31, "VO2": 34.1, "RQ": 0.75, "WQ": 0, "VCO2": 25.575, "SMRglu": 0, "SMRglu_prod": 0}, "pos": [200,-400]})
+systemic_organ_table.insert({"name": "Diaphragm", "functions": {"VO2": f_VO2, "SMRCO2": f_SMRCO2, "Venous O2": f_ven_O2, "Venous CO2": f_ven_CO2, "Venous Lac": f_ven_lac},
+                             "vars" : {"BF": 0.01, "SMRO2": 1, "Organ_Weight" : 0.31, "VO2": 3, "SMRglu": 0, "SMRglu_prod": 0, "RQ": 1}, "pos": [200, 400]})
+systemic_organ_table.insert({"name": "Kidneys", "functions": {"VO2": f_VO2, "VCO2": f_VCO2, "SMRCO2": f_SMRCO2, "Venous Glucose": f_ven_glu, "Venous O2": f_ven_O2, "Venous CO2": f_ven_CO2, "Venous Lac": f_ven_lac},
+                             "vars" : {"BF": 1, "SMRO2": 6.8, "Organ_Weight" : 0.29, "VO2": 19.72, "RQ": 0.85, "VCO2": 16.762,  "SMRglu": 0, "SMRglu_prod": 0}, "pos": [200,-500]})
+systemic_organ_table.insert({"name": "Spleen", "functions": {},
+                             "vars": {"Organ_Weight" : 0.14}, "pos": [200,500]})
+systemic_organ_table.insert({"name": "Pancreas", "functions": {},
+                             "vars": {"Organ_Weight" : 0.1}, "pos": [200,-600]})
+systemic_organ_table.insert({"name": "Thyroid", "functions": {},
+                             "vars": {"BF": 0.001, "Organ_Weight" : 0.015}, "pos": [200,-600]})
 
 #organ = Organ(systemic_organ_table.all()[0], global_param_table.all(), global_constant_table.all())
--- db/function_parser.py
+++ db/function_parser.py
@@ -1,3 +1,6 @@
+from PyQt5.QtWidgets import QMessageBox
+
+
 class EvalWrapper(object):
     def __init__(self, variables : dict):
         self.variables = variables
@@ -14,5 +17,15 @@ class EvalWrapper(object):
                 return eval(self.function, self.variables)
             except NameError:
                 return None
+            except ZeroDivisionError:
+                # On division by zero we will simply return 0 as an answer
+                msg = QMessageBox()
+                msg.setWindowTitle("Error")
+                printable_vars = self.variables
+                printable_vars.pop('__builtins__', None)
+                variables = {str(x) + ": " + str(self.variables[x]) + "\n" for x in printable_vars.keys()}
+                msg.setText("Division by zero, set result of " + self.function + " to 0\n" + "".join(variables))
+                msg.exec_()
+                return 0
         else:
             raise NameError("Cannot evaluate: no function is defined")
